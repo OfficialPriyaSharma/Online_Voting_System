1486fc4a6ad9717d2bb98d9498b3141658169f55
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-03-04 23:37:13 +0530

    Online Voting System

--- src/VotingSystem.java
+++ src/VotingSystem.java
@@ -7,25 +7,27 @@ public class VotingSystem extends JFrame {
 
     VotingSystem(){
         setBounds(0,0,1600,700);
+        getContentPane().setBackground(Color.WHITE);
 
         JLabel l1 = new JLabel("Welcome to");
-
         l1.setFont(new Font("Tahoma", Font.BOLD,50));
-        l1.setLayout(null);
         l1.setBounds(150,230,300,100);
         add(l1);
 
         JLabel l2 = new JLabel("Online Voting System");
         l2.setFont(new Font("Tahoma", Font.BOLD,50));
-        l2.setLayout(null);
-        l2.setBounds(400,300,500,100);
+        l2.setBounds(50,300,700,100);
         add(l2);
 
+        ImageIcon il = new ImageIcon(ClassLoader.getSystemResource("Online Voting System/src/icons/Vote.png"));
+        Image i2 = il.getImage().getScaledInstance(500,300,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l3 = new JLabel(i3);
+        l3.setBounds(700,200,500,300);
+        add(l3);
 
-        l1.setBounds(100,230,300,100);
-
-        setVisible(true);
         setLayout(null);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
