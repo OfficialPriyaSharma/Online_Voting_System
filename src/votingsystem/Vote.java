02b37e2cf666ff9205517a93fb6702276d584629
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-03-06 14:33:45 +0530

    Online Voting System

--- src/votingsystem/Vote.java
+++ src/votingsystem/Vote.java
@@ -7,31 +7,31 @@ public class Vote extends JFrame {
 
     Vote(){
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("votingsystem/icons/Shivsena.png"));
-        Image i2 = i1.getImage().getScaledInstance(400,300,Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(300,200,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l1 = new JLabel(i3);
-        l1.setBounds(50,50,400,300);
+        l1.setBounds(50,50,300,200);
         add(l1);
 
         ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("votingsystem/icons/AamAadmi.png"));
-        Image i5 = i4.getImage().getScaledInstance(400,300,Image.SCALE_DEFAULT);
+        Image i5 = i4.getImage().getScaledInstance(300,200,Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
         JLabel l2 = new JLabel(i6);
-        l2.setBounds(800,50,400,300);
+        l2.setBounds(800,50,300,200);
         add(l2);
 
         ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("votingsystem/icons/BJP.png"));
-        Image i8 = i7.getImage().getScaledInstance(400,300,Image.SCALE_DEFAULT);
+        Image i8 = i7.getImage().getScaledInstance(300,200,Image.SCALE_DEFAULT);
         ImageIcon i9 = new ImageIcon(i8);
         JLabel l3 = new JLabel(i9);
-        l3.setBounds(800,350,400,300);
+        l3.setBounds(800,350,300,200);
         add(l3);
 
         ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("votingsystem/icons/Congress.png"));
-        Image i11 = i10.getImage().getScaledInstance(400,300,Image.SCALE_DEFAULT);
+        Image i11 = i10.getImage().getScaledInstance(300,200,Image.SCALE_DEFAULT);
         ImageIcon i12 = new ImageIcon(i11);
         JLabel l4 = new JLabel(i12);
-        l4.setBounds(50,350,400,300);
+        l4.setBounds(50,350,300,200);
         add(l4);
 
         setBounds(0,0,1600,700);
